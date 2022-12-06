7f49953023b377fbf17c140a01eebdf9459d8843
Yung <neiklotrr@gmail.com>
2022-12-06 21:44:26 +0300

    Added global context

---
+++ context/StateContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { toast } from 'react-hot-toast'
+
+const Context = createContext()
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false)
+    const [cartItems, setCartItems] = useState()
+    const [totalPrice, setTotalPrice] = useState()
+    const [totalQuantites, setTotalQuantities] = useState()
+    const [qty, setQty] = useState(1)
+
+    return(
+        <Context.Provider value={{
+            showCart,
+            cartItems,
+            totalPrice,
+            totalQuantites,
+            qty,
+        }}>
+            { children }
+        </Context.Provider>
+    )
+}
